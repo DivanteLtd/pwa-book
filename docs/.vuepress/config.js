773af1884141380ffa1966a9acc6272dc0490266
Aleksandra Kwiecień <akwiecien@divante.com>
2019-09-20 12:48:13 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,9 +4,13 @@ module.exports = {
     base: '/pwabook/chapter/',
     themeConfig: {
         logo: '/assets/logo.png',
+        
         sidebar: [
 			['/chapter1','Chapter 1: INTRODUCTION TO PROGRESSIVE WEB APPS'],	
 			['/chapter2','Chapter 2: THE HISTORY OF PWA DEVELOPMENT']
-        ]
+        ],
+        nav: [
+         { text: 'Twitter', link: 'https://twitter.com/divanteltd' },
+       ]
     }
 }
