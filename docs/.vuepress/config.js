56a234fdab54f4d0249f98cba75cd28cf43b419d
Aleksandra Kwiecień <akwiecien@divante.com>
2019-09-24 14:29:14 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,13 +6,13 @@ module.exports = {
         logo: '/assets/logo_light_blue.png',
         
         sidebar: [
-			['/chapter1','Chapter 1: INTRODUCTION TO PROGRESSIVE WEB APPS'],	
-			['/chapter2','Chapter 2: THE HISTORY OF PWA DEVELOPMENT'],
-			['/chapter3','Chapter 3: PROGRESSIVE WEB APPS IN THE MOBILE-FIRST WORLD'],	
-			['/chapter4','Chapter 5: LOW MOBILE CONVERSION RATES SOLVED WITH PWA TECHNOLOGY'],	
-			['/chapter5','Chapter 1: INTRODUCTION TO PROGRESSIVE WEB APPS'],	
-			['/chapter6','Chapter 6: COSTS AND SOLUTIONS OF PWA IN ECOMMERCE'],
-			['/chapter7','Chapter 7: COMPANIES THAT IMPLEMENTED PWA AND WON']	
+			['/chapter1','1: INTRODUCTION TO PROGRESSIVE WEB APPS'],	
+			['/chapter2','2: THE HISTORY OF PWA DEVELOPMENT'],
+			['/chapter3','3: PROGRESSIVE WEB APPS IN THE MOBILE-FIRST WORLD'],	
+			['/chapter4','4: BENEFITS OF PROGRESSIVE WEB APPS'],	
+			['/chapter5','5: LOW MOBILE CONVERSION RATES SOLVED WITH PWA TECHNOLOGY'],
+			['/chapter6','6: COSTS AND SOLUTIONS OF PWA IN ECOMMERCE'],
+			['/chapter7','7: COMPANIES THAT IMPLEMENTED PWA AND WON']	
         ],
         nav: [
          { text: 'Twitter', link: 'https://twitter.com/divanteltd' },
