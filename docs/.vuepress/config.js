30412fde1e5fb87fb74879f5e9ac9d4dae2435d3
Aleksandra Kwiecień <akwiecien@divante.com>
2019-09-24 13:25:22 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,12 @@ module.exports = {
         
         sidebar: [
 			['/chapter1','Chapter 1: INTRODUCTION TO PROGRESSIVE WEB APPS'],	
-			['/chapter2','Chapter 2: THE HISTORY OF PWA DEVELOPMENT']
+			['/chapter2','Chapter 2: THE HISTORY OF PWA DEVELOPMENT'],
+			['/chapter3','Chapter 3: PROGRESSIVE WEB APPS IN THE MOBILE-FIRST WORLD'],	
+			['/chapter4','Chapter 5: LOW MOBILE CONVERSION RATES SOLVED WITH PWA TECHNOLOGY'],	
+			['/chapter5','Chapter 1: INTRODUCTION TO PROGRESSIVE WEB APPS'],	
+			['/chapter6','Chapter 6: COSTS AND SOLUTIONS OF PWA IN ECOMMERCE'],
+			['/chapter7','Chapter 7: COMPANIES THAT IMPLEMENTED PWA AND WON']	
         ],
         nav: [
          { text: 'Twitter', link: 'https://twitter.com/divanteltd' },
