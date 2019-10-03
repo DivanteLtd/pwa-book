be1e54149fc0929fbdf40a74bc8c3f330eaf7d03
Aleksandra Kwiecień <akwiecien@divante.com>
2019-10-03 12:24:33 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,6 +9,14 @@ module.exports = {
           ['link', { rel: 'icon', href: '/assets/favicon.png' }],
         ],
         
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5RZ9N2H');</script>
+        <!-- End Google Tag Manager -->
+        
         sidebar: [
 			['/01-Introduction-to-PWA-technology','1: Introduction to Progressive Web Apps'],	
 			['/02-The-history-of-PWAs','2: The history of PWAs'],
