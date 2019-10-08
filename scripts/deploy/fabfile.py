0bf91c6ec0585454e537ee837c6d368d5e9c01d5
fszenborn <54931392+fszenborn@users.noreply.github.com>
2019-10-08 16:20:32 +0200

    Update fabfile.py

--- scripts/deploy/fabfile.py
+++ scripts/deploy/fabfile.py
@@ -52,9 +52,9 @@ def deploy():
     print(colors.green("Start deploying in ") + colors.red(env.name))
     notify("*" + PROJECT_NAME + "* @ `" + env.name + "` :: Deployment Started")
 
-    #copy_chapters()
+    copy_chapters()
     
-    run("ls -la")
+    #run("ls -la")
 
     notify("*" + PROJECT_NAME + "* @ `" + env.name + "` :: Deployment Completed")
 
