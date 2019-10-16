1367bd1a43479a0afd69488f8e1587e5028b3b8e
Aleksandra Kwiecień <olakwiecien@gmail.com>
2019-10-16 12:50:02 +0200

    aktualizacja rozd 5

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,7 @@ module.exports = {
           ['/02-The-history-of-PWAs','2. The history of PWAs'],
           ['/03-PWAs-in-the-mobile-first-world','3. PWA in the mobile-first world'],
           ['/04-Benefits-of-PWA','4. Benefits of Progressive Web Apps'],
-          ['/05-Improving-mobile-CR-with-PWAs','5. Low mobile conversion rates solved with PWA technology TBA'],
+          ['/05-Improving-mobile-CR-with-PWAs','5. Low mobile conversion rates solved with PWA technology NEW'],
           ['/06-PWA-solutions-and-costs','6. Costs and solutions of PWA in eCommerce TBA'],
           ['/07-PWA-success-stories','7. Companies that implemented PWA and won TBA']
         ],
